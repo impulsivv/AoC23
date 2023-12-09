76afa2bd5a4056d72d93685de5aad490db9ba154
Imp <artemon.d@mail.ru>
2023-12-09 11:13:35 +0100

    solve day3 with regex

--- day3/Program.cs
+++ day3/Program.cs
@@ -10,96 +10,46 @@ namespace day1
 {
     class Program
     {
-        static void GetSurroundedCoords((int srow, int scol) sChar, ref HashSet<(int row, int col)> coords)
-        {
-            for (int row = -1; row < 2; row++)
-                for (int col = -1; col < 2; col++)
-                    coords.Add( (sChar.srow + row, sChar.scol + col) );
-        }
-
-        static bool CheckIfNeeded(Match m, HashSet<(int row, int col)> coords, int row)
-        {
-            for (int col = 0; col < m.Length; col++)
-                if (coords.Contains( (row, m.Index + col) ))
-                    return true;
-
-            return false;
-        }
-
-        static int CheckIfTwoAround(MatchCollection m1, MatchCollection m2, MatchCollection m3, (int row, int col) coord)
-        {
-            HashSet<(int row, int col)> coords = new();
-            GetSurroundedCoords(coord, ref coords);
+        static int GetTouchingNumbers(int row, string[] lines, int starCol, int maxTouch=0)
+        {   
+            Regex regexNumbers = new Regex(@"\d+");
             List<int> results = new();
-
-            foreach (Match m in m1)
-                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(coord.col -1, 3)).Count() != 0)
+            foreach (Match m in regexNumbers.Matches(lines[row-1]))
+                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(starCol -1, 3)).Count() != 0)
                     results.Add(int.Parse(m.Value));
-            foreach (Match m in m2)
-                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(coord.col -1, 3)).Count() != 0)
+            foreach (Match m in regexNumbers.Matches(lines[row]))
+                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(starCol -1, 3)).Count() != 0)
                     results.Add(int.Parse(m.Value));
-            foreach (Match m in m3)
-                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(coord.col -1, 3)).Count() != 0)
+            foreach (Match m in regexNumbers.Matches(lines[row+1]))
+                if(Enumerable.Range(m.Index , m.Length).Intersect(Enumerable.Range(starCol -1, 3)).Count() != 0)
                     results.Add(int.Parse(m.Value));
-
-            if (results.Count == 2)
+            
+            if (maxTouch > 0 && maxTouch == results.Count)
                 return results.Aggregate(1, (int accu, int next) => accu * next);
-            else return 0;
-
+            else if (maxTouch == 0)
+                return results.Aggregate(0, (int accu, int next) => accu + next);
+            else 
+                return 0;
         }
         //x - 48
         static void Main(string[] args)
         {
             string input = System.IO.File.ReadAllText(@"./input.txt").Replace("\r", string.Empty);
             string[] lines = input.Split("\n");
-
-            char[][] matrix = lines.Select(x => x.ToArray()).ToArray();
-            char[] sChars = new char[]{'/','*','%','+','-','#','&','=','$','@'};
+            Regex regexStar = new Regex(@"\*");
+            Regex regexSChars = new Regex(@"\*|/|%|\+|-|#|&|=|\$|@");
             //part1
             int p1Result = 0;
-            HashSet<(int row, int col)> coords = new();
-            
-            for (int row = 0; row < matrix.Length; row++)
-                for (int col = 0; col < matrix[row].Length; col++)
-                    if (sChars.Contains(matrix[row][col])) 
-                        GetSurroundedCoords((row, col), ref coords);
-
-            Regex regexNumbers = new Regex(@"\d+");
-            for (int row = 0; row < lines.Length; row++)
-                foreach (Match m in regexNumbers.Matches(lines[row]))
-                    p1Result += CheckIfNeeded(m, coords, row) ? int.Parse(m.Value) : 0;
-
+            //no special chars on first and last row anyways :)
+            for (int row = 1; row < lines.Length - 1; row++)
+                foreach (Match m in regexSChars.Matches(lines[row]))
+                    p1Result += GetTouchingNumbers(row, lines, m.Index); 
             Console.WriteLine(p1Result);
-
-
-
-            
             //part2
             int p2Result = 0;
-            HashSet<(int row, int index, int len)> coordsNums = new();
-
-            Regex regexStar = new Regex(@"\*");
             for (int row = 1; row < lines.Length - 1; row++)
-            {   
-                //var nums = regexNumbers.Matches(lines[row]);
-                //foreach (Match m in nums) coordsNums.Add( (row, m.Index, m.Length) );
-                    //p2Result += CheckIfNeeded(m, coords, row) ? int.Parse(m.Value) : 0;
-
-                var stars = regexStar.Matches(lines[row]);
-                foreach (Match m in stars)
-                {
-                    (int row, int col) starCoord = (row, m.Index);
-                    string row1 = lines[row-1];
-                    string row2 = lines[row];
-                    string row3 = lines[row+1];
-                    var nums1 = regexNumbers.Matches(row1);
-                    var nums2 = regexNumbers.Matches(row2);
-                    var nums3 = regexNumbers.Matches(row3);
-
-                    p2Result += CheckIfTwoAround(nums1,nums2,nums3,starCoord); 
-
-                }
-            }
+                foreach (Match m in regexStar.Matches(lines[row]))
+                    p2Result += GetTouchingNumbers(row, lines, m.Index, 2); 
             Console.WriteLine(p2Result);
     
         }
